d3b5b26b812fac1268e9f2f05b4a58a7a99bee06
andre <andreamusholin@gmail.com>
2025-04-18 10:06:03 +0700

    menambahkan Dockerfile dan .cloudbuild.yaml

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -13,19 +13,7 @@ if (!admin.apps.length) {
 
   admin.initializeApp({
     credential: admin.credential.cert(
-      // `{
-      //   "type": "service_account",
-      //   "project_id": "${process.env.ADMIN_PROJECT_ID}",
-      //   "private_key_id": "${process.env.ADMIN_PRIVATE_KEY_ID}",
-      //   "private_key": "${process.env.ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n")}",
-      //   "client_email": "${process.env.ADMIN_CLIENT_EMAIL}",
-      //   "client_id": "${process.env.ADMIN_CLIENT_ID}",
-      //   "auth_uri": "${process.env.ADMIN_AUTH_URI}",
-      //   "token_uri": "${process.env.ADMIN_TOKEN_URI}",
-      //   "auth_provider_x509_cert_url": "${process.env.ADMIN_AUTH_PROVIDER_X509_CERT_URL}",
-      //   "client_x509_cert_url": "${process.env.ADMIN_CLIENT_X509_CERT_URL}",
-      //   "universe_domain": "${process.env.ADMIN_UNIVERSE_DOMAIN}"
-      // }`
+      
         {
         projectId: process.env.ADMIN_PROJECT_ID,
         // privateKeyId: process.env.ADMIN_PRIVATE_KEY_ID,
@@ -39,3 +27,17 @@ if (!admin.apps.length) {
 
 
 export const db = admin.firestore();
+
+// `{
+      //   "type": "service_account",
+      //   "project_id": "${process.env.ADMIN_PROJECT_ID}",
+      //   "private_key_id": "${process.env.ADMIN_PRIVATE_KEY_ID}",
+      //   "private_key": "${process.env.ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n")}",
+      //   "client_email": "${process.env.ADMIN_CLIENT_EMAIL}",
+      //   "client_id": "${process.env.ADMIN_CLIENT_ID}",
+      //   "auth_uri": "${process.env.ADMIN_AUTH_URI}",
+      //   "token_uri": "${process.env.ADMIN_TOKEN_URI}",
+      //   "auth_provider_x509_cert_url": "${process.env.ADMIN_AUTH_PROVIDER_X509_CERT_URL}",
+      //   "client_x509_cert_url": "${process.env.ADMIN_CLIENT_X509_CERT_URL}",
+      //   "universe_domain": "${process.env.ADMIN_UNIVERSE_DOMAIN}"
+      // }`
